ad0b3a664469e35831bcd2ada3460f0eb5f16a12
hi-rachel <rachel.uiux@gmail.com>
2022-11-16 23:48:53 +0900

    Multi Boards

---
+++ src/Components/Board.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Droppable } from "react-beautiful-dnd";
+import DraggableCard from "./DraggableCard";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 20px 10px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+interface IBoardProps {
+  toDos: string[];
+  boardId: string;
+}
+
+function Board({ toDos, boardId }: IBoardProps) {
+  return (
+    <Droppable droppableId="boardId">
+      {(provided) => (
+        <Wrapper ref={provided.innerRef} {...provided.droppableProps}>
+          {toDos.map((toDo, index) => (
+            <DraggableCard key={toDo} index={index} toDo={toDo} />
+          ))}
+          {provided.placeholder}
+        </Wrapper>
+      )}
+    </Droppable>
+  );
+}
+
+export default Board;
